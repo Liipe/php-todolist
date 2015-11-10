fe43975c162555bf886a858707dd40c15e881a48
liipe <luiz-dasilva@hotmail.com>
2015-11-10 22:32:49 +0000

    add api/tasks

--- api/index.php
+++ api/index.php
@@ -9,9 +9,20 @@ $app->get('/',function() use ($app) {
     echo "Welcome to REST API";
 });
 
-$app->get('/hello/:name', function($name) use ($app) {
-    echo "Hi $name, welcome to the REST API's";
+$app->get('/tasks', function() use ($app) {
+    $tasks = getTasks();
+    
+        echo json_encode($tasks);
 });
 
+//TODO move it to a DAO class
+function getTasks(){
+        $tasks = array(
+            array('id'=>1,'description'=>'Learn REST','done'=>false),
+            array('id'=>2,'description'=>'Learn JavaScript','done'=>false)
+        );
+        
+        return $tasks;
+}
 $app->run();
 ?>
