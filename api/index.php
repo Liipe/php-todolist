4f5c2f6fe2a2c49393e51433a10ac49448e3f0c9
liipe <luiz-dasilva@hotmail.com>
2015-11-10 22:01:15 +0000

    added apache configuration

--- api/index.php
+++ api/index.php
@@ -4,6 +4,7 @@ require 'vendor/autoload.php';
 $app = new \Slim\Slim();
 
 // Quando o / for requisitado a funçao sera chamada e a $app será usada.
+//http://hostname/api
 $app->get('/',function() use ($app) {
     echo "Welcome to REST API";
 });
